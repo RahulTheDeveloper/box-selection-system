3516591d805ccc70b1da4179d949678052d4c328
RahulTheDeveloper <rahulkrkashyap9899@gmail.com>
2026-09-21 10:56:13 +0530

    Create product box and order apps

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'config'
+    "products",
+    "boxes",
+    "orders",
+    
 ]
 
 MIDDLEWARE = [
